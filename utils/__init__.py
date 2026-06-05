1df062085088468fa390779c61d76c56c2dc2f40
qenev <benbesicc@gmail.com>
2026-06-05 16:54:57 -0700

    chore: setup configuration 30

---
+++ utils/__init__.py
@@ -0,0 +1,31 @@
+"""Utility modules for the hand AR tracker system.
+
+This package contains helper modules for FPS calculation,
+mathematical operations, and compute device management.
+"""
+
+from utils.fps_counter import FPSCounter
+from utils.math_utils import (
+    calculate_distance,
+    calculate_angle,
+    normalize_vector,
+    landmark_to_pixel,
+    smooth_landmarks,
+)
+from utils.device_utils import (
+    list_available_devices,
+    select_device,
+    get_device_label,
+)
+
+__all__ = [
+    "FPSCounter",
+    "calculate_distance",
+    "calculate_angle",
+    "normalize_vector",
+    "landmark_to_pixel",
+    "smooth_landmarks",
+    "list_available_devices",
+    "select_device",
+    "get_device_label",
+]
