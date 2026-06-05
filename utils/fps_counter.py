ad59d37b9ace33f6936c24b29e0bf803c281ad9e
qenev <benbesicc@gmail.com>
2026-06-05 16:56:18 -0700

    feat: average frame rate calculation 11

--- utils/fps_counter.py
+++ utils/fps_counter.py
@@ -18,3 +18,5 @@ class FPSCounter:
 
     Attributes:
         window_size: Number of frames to include in the rolling average.
+        timestamps: Deque of frame arrival timestamps.
+        min_fps: Lowest FPS value observed since last reset.
