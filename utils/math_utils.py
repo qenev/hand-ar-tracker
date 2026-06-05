cb2f88e599df77b40d21920259f054a5da0b425c
qenev <benbesicc@gmail.com>
2026-06-05 16:56:10 -0700

    feat: add vector operation 25

--- utils/math_utils.py
+++ utils/math_utils.py
@@ -70,3 +70,6 @@ def calculate_angle(
         point_a[1] - point_b[1],
         point_a[2] - point_b[2],
     )
+    vector_bc = (
+        point_c[0] - point_b[0],
+        point_c[1] - point_b[1],
