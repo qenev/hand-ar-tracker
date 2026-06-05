dc76be17b2d1be433bdb4fb192978e090ac7adfd
qenev <benbesicc@gmail.com>
2026-06-05 16:55:38 -0700

    feat: detect extension state 108

--- gesture.py
+++ gesture.py
@@ -212,3 +212,5 @@ def _is_peace_sign(finger_states: List[bool]) -> bool:
     middle_extended = finger_states[2]
     ring_curled = not finger_states[3]
     pinky_curled = not finger_states[4]
+    return (
+        index_extended
