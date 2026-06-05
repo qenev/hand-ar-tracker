efe6478398eb0e67c50eef6bc531fb15f29f73fb
qenev <benbesicc@gmail.com>
2026-06-05 16:55:16 -0700

    feat: draw joints and meshes 26

--- renderer.py
+++ renderer.py
@@ -98,3 +98,7 @@ class HandRenderer:
         pixel_coords = self._compute_pixel_coords(
             landmarks, width, height
         )
+        frame = self._draw_skeleton(frame, pixel_coords)
+        frame = self._draw_keypoints(frame, pixel_coords)
+        if self.show_hand_label and hand_label:
+            frame = self._draw_hand_label(
